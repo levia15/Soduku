cdf3e83515eec183fd31664a2083a998420124a9
Anais <anais.levi20@gmail.com>
2023-12-05 16:05:09 -0500

    Add generate button

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 export default function Layout() {
+    const [soduku, setSoduku] = useState('')
+    const getSoduku = () => { }
+    
+   
     return (
-        <p>Test</p>
+        <>
+            <Button onClick={getSoduku}>Generate</Button>
+        </>
     )
 }
