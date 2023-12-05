096b5fffc6b4b4d9c6de3ab00290ccd7bce9747e
Anais <anais.levi20@gmail.com>
2023-12-05 16:25:04 -0500

    get api call

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export default function Layout() {
-    const [soduku, setSoduku] = useState('')
-    const getSoduku = () => { }
+    const [soduku, setSoduku] = useState([]);
+    const getSoduku = () => {
+        axios.get('https://sudokucollective.com/api/v1/games/createannonymous', { params: { DifficultyLevel: 2 } })
+            .then(response => {
+                setSoduku(response.data.payload[0].rows);
+                console.log(response.data.payload[0].rows);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+
+     }
     
    
     return (
