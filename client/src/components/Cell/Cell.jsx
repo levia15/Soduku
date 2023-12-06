35e6b025e9938523c5a4d20b39d110e6afef131b
Anais <anais.levi20@gmail.com>
2023-12-06 16:25:07 -0500

    refactor thickening third column

--- client/src/components/Cell/Cell.jsx
+++ client/src/components/Cell/Cell.jsx
@@ -5,8 +5,7 @@ export default function Cell(props) {
     const cellCol = props.cellCol;
     return (
         <>
-        {(cellCol+1)%3==0 ? 
-            <span className='sudoku-cell-thick'> {value !==0 ? value : ''}</span> : <span className='sudoku-cell'> {value !==0 ? value : ''}</span>}
+            <span className={(cellCol+1)%3==0 ? "sudoku-cell-thick" : "sudoku-cell"}> {value !==0 ? value : ''}</span>
         </>
     )
 }
