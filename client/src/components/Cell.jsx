3e9957bd6ab13df3edaeb6f0ac74193bdb7dd7c7
Anais <anais.levi20@gmail.com>
2023-12-06 15:40:33 -0500

    display sudoku board grid

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
 
 export default function Cell(props) {
     const cell = props.cell;
     return (
         <>
-           <Card bg='light' style={{ width: '3rem' }} border='dark'>
-                <Card.Body>{cell}</Card.Body>
-           </Card>
+           <span className='sudoku-cell'>{cell}</span>
         </>
     )
 }
--- client/src/components/Puzzle.jsx
+++ client/src/components/Puzzle.jsx
@@ -1,19 +1,21 @@
 import React from "react"
 import Cell from "./Cell";
+import "./Cell.css"
+import "./Puzzle.css"
 
 export default function Puzzle(props) {
     const puzzle = props.puzzle;
-    console.log(puzzle);
     return (
         <>
-            {puzzle.map((row, r) => (
-                <div key={r}>
-                    {row.map((cell, c) => (
-                        //<span key={c}>{cell}</span>
-                        <Cell cell={cell}></Cell>
-                    ))}
-                </div>
-            ))}
+            <div className="sudoku-board">
+                {puzzle.map((row, r) => ( 
+                    <div className="sudoku-row">
+                        {row.map((cell, c) => (
+                            <Cell cell={cell}></Cell>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
