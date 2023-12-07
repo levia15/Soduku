d8827d647f511baa6775905ba9a59cb6bd40aaf6
Anais <anais.levi20@gmail.com>
2023-12-07 12:58:18 -0500

    Only allow [1-9] input into cells

--- client/src/components/Cell/Cell.jsx
+++ client/src/components/Cell/Cell.jsx
@@ -1,13 +1,27 @@
 import React, {useState} from "react";
 
 export default function Cell(props) {
-    const value = props.value;
-    const cellCol = props.cellCol;
+    const [value, setValue] = useState(props.value !==0 ? props.value :'');
+    const [isEditable] = useState(props.value !==0 ? false : true);
+    const cellCol = props.cellCol; 
     const cellRow = props.cellRow;
+
+    const setValidInput = (e) => {
+        const inputValue = e.target.value;
+
+        if (/^[1-9]$/.test(inputValue) || inputValue === '') {
+            setValue(inputValue)
+        }
+    }
 
     return (
         <>
-            <input readonly={value == 0 ? false : 'readonly'} placeholder={props.value !==0 ? value : ''} className={(cellCol==2 || cellCol==5)? "sudoku-cell-thick" : "sudoku-cell"}/>
+            <input 
+                onInput={setValidInput} 
+                readonly={isEditable ? false : 'readonly'} 
+                value={value} 
+                className={(cellCol==2 || cellCol==5)? "sudoku-cell-thick" : "sudoku-cell"}
+            />
         </>
     )
 }
